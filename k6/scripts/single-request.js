34171b33a8e4dd7bb27b080a6079399a52b37e81
Emmanuel Medina <emmanuel.medina@univ-lorraine.fr>
2024-04-13 09:25:02 +0200

    k6

--- k6/scripts/single-request.js
+++ k6/scripts/single-request.js
@@ -2,9 +2,9 @@
 import http from "k6/http";
 
 export const options = {
-  iterations: 1,
+  iterations: 100,
 };
 
 export default function () {
-  const response = http.get("https://whoami/bench");
+  const response = http.get("http://whoami/bench");
 }
