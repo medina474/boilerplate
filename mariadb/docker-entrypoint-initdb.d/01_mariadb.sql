723172c1756467c1d7f26034e8a3d3f4d611f52c
emedina <emmanuel.medina@univ-lorraine.fr>
2024-04-17 08:57:24 +0200

    mysql exporter

--- mariadb/docker-entrypoint-initdb.d/01_mariadb.sql
+++ mariadb/docker-entrypoint-initdb.d/01_mariadb.sql
@@ -1,6 +1,6 @@
 -- exporter
-create user 'exporter'@'localhost' identified by 'myPassword' with max_user_connections 3;
-grant process, replication client, select on *.* to 'exporter'@'localhost';
+create user 'exporter'@'%' identified by 'myPassword' with max_user_connections 2;
+grant process, replication client, slave monitor, select on *.* to 'exporter'@'%';
 
 -- phpMyAdmin
 create database phpmyadmin;
