b2493235fb6eabd55d00a8cb5f7ba50566a6ae02
Emmanuel Medina <emmanuel.medina@univ-lorraine.fr>
2024-04-19 07:04:58 +0200

    portainer

--- mariadb/docker-entrypoint-initdb.d/01_mariadb.sql
+++ mariadb/docker-entrypoint-initdb.d/01_mariadb.sql
@@ -3,4 +3,6 @@ create user 'exporter'@'%' identified by 'myPassword' with max_user_connections 
 grant process, replication client, slave monitor, select on *.* to 'exporter'@'%';
 
 -- phpMyAdmin
+create user 'pma'@'phpmyadmin' identified by 'motdepasse';
 create database phpmyadmin;
+grant all privileges on `phpmyadmin`.* to 'pma'@'phpmyadmin';
